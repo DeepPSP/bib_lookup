7b9a301bb68110ca301b3aca1283a523debcea71
WEN Hao <wenh06@gmail.com>
2022-04-07 22:55:26 +0800

    add test files for the function of bib items checking

---
+++ test/test_checking.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+try:
+    import bib_lookup
+except ModuleNotFoundError:
+    import sys
+
+    sys.path.insert(0, str(Path(__file__).parent.parent.resolve()))
+    import bib_lookup
+
+
+_CWD = Path(__file__).absolute().parent
+
+_INPUT_FILE = _CWD / "invalid_items.txt"
+
+
+def test_checking():
+    bl = bib_lookup.BibLookup()
+    err_lines = bl.check_bib_file(_INPUT_FILE)
+    assert err_lines == [3, 16, 45]
