5a002bb5975194c9d8680ddd496d2ca3b7cae6e4
WEN Hao <wenh06@gmail.com>
2022-12-03 01:08:08 +0800

    add test_bibitem.py

---
+++ test/test_bibitem.py
@@ -0,0 +1,123 @@
+"""
+"""
+
+import itertools
+from collections import OrderedDict
+
+import pytest
+
+from bib_lookup._bib import BibItem, BIB_ENTRY_TYPES, BIB_FIELDS
+
+
+class TestBibItem:
+
+    identifier = "10.1088/1361-6579/ac9451"
+    entry_type = "article"
+    fields = OrderedDict(
+        title="A Novel Deep Learning Package for Electrocardiography Research",
+        author="Hao Wen and Jingsu Kang",
+        journal="Physiological Measurement",
+        year=2022,
+        month=11,
+        publisher="IOP Publishing",
+        volume=43,
+        number=11,
+        pages=115006,
+    )
+
+    bibitem = BibItem(
+        identifier=identifier,
+        entry_type=entry_type,
+        fields=fields,
+        label="xxx",
+    )
+
+    def test_eq(self):
+        grid = itertools.product(
+            [None, "10.1088/1361-6579/ac9451", "torch_ecg_paper"],  # label
+            ["middle", "left", "left-middle", "left_middle"],  # align
+            [True, False],  # check_fields
+        )
+
+        for label, align, check_fields in grid:
+            bibitem_1 = BibItem(
+                identifier=self.identifier,
+                entry_type=self.entry_type,
+                fields=self.fields,
+                label=label,
+                align=align,
+                check_fields=check_fields,
+            )
+            print(bibitem_1)
+            assert bibitem_1 == bibitem_1
+            assert bibitem_1 != self.bibitem
+            assert self.bibitem != bibitem_1
+            assert bibitem_1.__eq__(self.bibitem, strict=True)
+            assert self.bibitem.__eq__(bibitem_1, strict=True)
+
+    def test_help(self):
+        for item in itertools.chain(BIB_ENTRY_TYPES, BIB_FIELDS):
+            BibItem.help(item)
+            self.bibitem.help(item)
+
+    def test_errors(self):
+        with pytest.raises(AssertionError, match="`xxx` is not a valid entry type"):
+            BibItem(identifier=self.identifier, entry_type="xxx", fields=self.fields)
+
+        with pytest.raises(
+            AssertionError, match="`fields` must be `OrderedDict`, but got `.+`"
+        ):
+            BibItem(
+                identifier=self.identifier,
+                entry_type=self.entry_type,
+                fields=dict(self.fields),
+            )
+
+        with pytest.raises(
+            AssertionError,
+            match="`align` must be one of \\['middle', 'left', 'left-middle', 'left_middle'\\], but got `xxx`",
+        ):
+            BibItem(
+                identifier=self.identifier,
+                entry_type=self.entry_type,
+                fields=self.fields,
+                align="xxx",
+            )
+
+        with pytest.raises(
+            AssertionError, match="required field \042.+\042 is missing"
+        ):
+            tmp = self.fields.copy()
+            tmp.pop("title")
+            tmp.pop("author")
+            BibItem(
+                identifier=self.identifier,
+                entry_type=self.entry_type,
+                fields=tmp,
+                check_fields=True,
+            )
+
+        with pytest.raises(
+            AssertionError,
+            match="required field\\(s\\) \042.+\042 is \\(are\\) missing",
+        ):
+            tmp = self.fields.copy()
+            tmp.pop("title")
+            tmp.pop("author")
+            BibItem(
+                identifier=self.identifier,
+                entry_type="book",
+                fields=tmp,
+                check_fields=True,
+            )
+
+        with pytest.raises(
+            AssertionError,
+            match="`entries_or_fields` must be of type `str` or a sequence, but got `.+`",
+        ):
+            BibItem.help(1)
+
+        with pytest.raises(
+            AssertionError, match="`xxx` is not a valid entry type or field name"
+        ):
+            BibItem.help("xxx")
