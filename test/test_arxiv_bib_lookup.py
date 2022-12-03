84429a4dc1366173bdabf2934103cc5eca39d644
WEN Hao <wenh06@gmail.com>
2022-12-03 12:25:00 +0800

    fix errors in test_arxiv_bib_lookup

--- test/test_arxiv_bib_lookup.py
+++ test/test_arxiv_bib_lookup.py
@@ -20,4 +20,3 @@ def test_arxiv_bib_lookup():
         bib_string = bl(arXiv_id, arxiv2doi=False, timeout=1000)
         assert bib_string == lookup_result, f"{arXiv_id} mismatch"
     bib_string = bl(arXiv_id, arxiv2doi=True, timeout=1000, verbose=10)
-    assert bib_string == lookup_result, f"{arXiv_id} mismatch"
