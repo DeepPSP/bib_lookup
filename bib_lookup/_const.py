f315c3aa0ddd9badd02f7cf8b286b2d3d14bed2f
WEN Hao <wenh06@gmail.com>
2023-04-27 23:03:56 +0800

    allow config file

--- bib_lookup/_const.py
+++ bib_lookup/_const.py
@@ -5,3 +5,21 @@ from pathlib import Path
 
 CACHE_DIR = Path.home() / ".cache" / "bib-lookup"
 CACHE_DIR.mkdir(parents=True, exist_ok=True)
+
+# user config file
+CONFIG_FILE = CACHE_DIR / "config.json"
+
+# default config
+DEFAULT_CONFIG = dict(
+    align="middle",
+    email=None,
+    ignore_fields=["url", "abstract"],
+    ignore_errors=False,
+    timeout=6.0,
+    arxiv2doi=True,
+    format="bibtex",
+    style="apa",
+    verbose=0,
+    print_result=False,
+    ordering=["title", "author", "journal", "booktitle"],
+)
